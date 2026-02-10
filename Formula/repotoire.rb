fadd6fadf8660ce6e96045211597482086b009b4
Zach Hammad <zachariahammad@gmail.com>
2026-02-10 15:51:39 -0500

    Update to v0.3.2

--- Formula/repotoire.rb
+++ Formula/repotoire.rb
@@ -1,17 +1,17 @@
 class Repotoire < Formula
   desc "Graph-powered code analysis with 81 detectors"
   homepage "https://github.com/Zach-hammad/repotoire"
-  version "0.3.1"
+  version "0.3.2"
   license "MIT"
 
   on_macos do
-    url "https://github.com/Zach-hammad/repotoire/releases/download/v0.3.1/repotoire-macos-aarch64.tar.gz"
-    sha256 "2081a682711895d0aba5b799b94e17148838ae888ea293d908276896ca8be8bf"
+    url "https://github.com/Zach-hammad/repotoire/releases/download/v0.3.2/repotoire-macos-aarch64.tar.gz"
+    sha256 "5dfc63346d400dd330ed7768c6c06594569e14afa1f5df2cf827084bab1ddb1d"
   end
 
   on_linux do
-    url "https://github.com/Zach-hammad/repotoire/releases/download/v0.3.1/repotoire-linux-x86_64.tar.gz"
-    sha256 "498997bbfccc6808962674d9f71949b1a0c70c89cbf72b733ea064ad1f73003f"
+    url "https://github.com/Zach-hammad/repotoire/releases/download/v0.3.2/repotoire-linux-x86_64.tar.gz"
+    sha256 "e554cc6a2d960270857eb74cb27399584aca9a3f1932224e31184b1aff8b68d1"
   end
 
   def install
