0ca5f98eee2dd46f85c6599b9474f7a17be77d34
Zach Hammad <zachariahammad@gmail.com>
2026-02-10 15:29:47 -0500

    Update SHA256 hashes for v0.3.1

--- Formula/repotoire.rb
+++ Formula/repotoire.rb
@@ -5,22 +5,13 @@ class Repotoire < Formula
   license "MIT"
 
   on_macos do
-    on_arm do
-      url "https://github.com/Zach-hammad/repotoire/releases/download/v0.3.1/repotoire-macos-aarch64.tar.gz"
-      sha256 "PLACEHOLDER"
-    end
-    on_intel do
-      # Intel Macs use ARM binary via Rosetta 2
-      url "https://github.com/Zach-hammad/repotoire/releases/download/v0.3.1/repotoire-macos-aarch64.tar.gz"
-      sha256 "PLACEHOLDER"
-    end
+    url "https://github.com/Zach-hammad/repotoire/releases/download/v0.3.1/repotoire-macos-aarch64.tar.gz"
+    sha256 "2081a682711895d0aba5b799b94e17148838ae888ea293d908276896ca8be8bf"
   end
 
   on_linux do
-    on_intel do
-      url "https://github.com/Zach-hammad/repotoire/releases/download/v0.3.1/repotoire-linux-x86_64.tar.gz"
-      sha256 "PLACEHOLDER"
-    end
+    url "https://github.com/Zach-hammad/repotoire/releases/download/v0.3.1/repotoire-linux-x86_64.tar.gz"
+    sha256 "498997bbfccc6808962674d9f71949b1a0c70c89cbf72b733ea064ad1f73003f"
   end
 
   def install
