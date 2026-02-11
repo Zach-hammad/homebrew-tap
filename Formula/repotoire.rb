50edd04d9a7b0bc4254a79865b63b18ea296bd84
Zach Hammad <zachariahammad@gmail.com>
2026-02-11 09:30:19 -0500

    Update repotoire to v0.3.34

--- Formula/repotoire.rb
+++ Formula/repotoire.rb
@@ -1,25 +1,25 @@
 class Repotoire < Formula
-  desc "Code analysis CLI with 81 detectors for security, architecture & quality"
+  desc "Graph-powered code analysis CLI - 81 detectors for security, architecture & quality"
   homepage "https://github.com/Zach-hammad/repotoire"
-  version "0.3.2"
+  version "0.3.34"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/Zach-hammad/repotoire/releases/download/v0.3.2/repotoire-macos-aarch64.tar.gz"
-      sha256 "5dfc63346d400dd330ed7768c6c06594569e14afa1f5df2cf827084bab1ddb1d"
+      url "https://github.com/Zach-hammad/repotoire/releases/download/v0.3.34/repotoire-macos-aarch64.tar.gz"
+      sha256 "f983d5ca7d624c6eedf13f6dc3a3eb4c3908473c5a84f7cb9c6f66d2f200605e"
     end
     on_intel do
-      # Intel Macs can use cargo install or Rosetta 2
-      url "https://github.com/Zach-hammad/repotoire/releases/download/v0.3.2/repotoire-macos-aarch64.tar.gz"
-      sha256 "5dfc63346d400dd330ed7768c6c06594569e14afa1f5df2cf827084bab1ddb1d"
+      # Intel Macs - use ARM binary with Rosetta 2
+      url "https://github.com/Zach-hammad/repotoire/releases/download/v0.3.34/repotoire-macos-aarch64.tar.gz"
+      sha256 "f983d5ca7d624c6eedf13f6dc3a3eb4c3908473c5a84f7cb9c6f66d2f200605e"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/Zach-hammad/repotoire/releases/download/v0.3.2/repotoire-linux-x86_64.tar.gz"
-      sha256 "e554cc6a2d960270857eb74cb27399584aca9a3f1932224e31184b1aff8b68d1"
+      url "https://github.com/Zach-hammad/repotoire/releases/download/v0.3.34/repotoire-linux-x86_64.tar.gz"
+      sha256 "64b8cd65926f0a30405f361a12b233e7901e72689e165e3195ec326b640fe2ac"
     end
   end
 
@@ -28,6 +28,6 @@ class Repotoire < Formula
   end
 
   test do
-    assert_match "repotoire #{version}", shell_output("#{bin}/repotoire --version")
+    system "#{bin}/repotoire", "--version"
   end
 end
