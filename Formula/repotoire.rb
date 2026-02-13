3857e5ac806ccadaf9f9c3665313f55ed33c6913
Zach Hammad <zachariahammad@gmail.com>
2026-02-12 19:08:06 -0500

    v0.3.99: Update SHA256 hashes

--- Formula/repotoire.rb
+++ Formula/repotoire.rb
@@ -1,28 +1,24 @@
 class Repotoire < Formula
   desc "Graph-powered code analysis with 108 detectors"
   homepage "https://github.com/Zach-hammad/repotoire"
-  version "0.3.98"
+  version "0.3.99"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/Zach-hammad/repotoire/releases/download/v#{version}/repotoire-macos-aarch64.tar.gz"
-      sha256 "PLACEHOLDER_MACOS_ARM64"
+      sha256 "b45995f8e36dcad19e698fb8919b5f12f8a84151dd02e21f10c972849fccb8ec"
     end
     on_intel do
       url "https://github.com/Zach-hammad/repotoire/releases/download/v#{version}/repotoire-macos-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_MACOS_X86_64"
+      sha256 "230d37a10754bd9e84483386aab79d70d293d8b8a22908029eb0d9d68e7521d2"
     end
   end
 
   on_linux do
-    on_arm do
-      url "https://github.com/Zach-hammad/repotoire/releases/download/v#{version}/repotoire-linux-aarch64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_ARM64"
-    end
     on_intel do
       url "https://github.com/Zach-hammad/repotoire/releases/download/v#{version}/repotoire-linux-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_X86_64"
+      sha256 "81da6be64c9b1b5d986cb14ade67169815b9b2ad5050564d23f971ace1de2d9b"
     end
   end
 
